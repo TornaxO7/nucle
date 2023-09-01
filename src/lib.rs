88277d1b1d207996851e1d0a18fabdd1db585a3c
TornaxO7 <tornax@proton.me>
2023-09-01 22:17:42 +0200

    adding walkdir

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,23 @@
 mod cli;
 
-use clap::Parser;
+use std::{io, env};
 
-#[derive(thiserror::Error, Debug, Clone, PartialEq, Eq, Hash)]
-pub enum NucleError {}
+use clap::Parser;
+use walkdir::WalkDir;
+
+#[derive(thiserror::Error, Debug)]
+pub enum NucleError {
+    #[error("IO-Error occured: {0}")]
+    IO(#[from] io::Error),
+}
 
 pub fn run() -> Result<(), NucleError> {
     let args = cli::Cli::parse();
+    let dir_walker = get_dir_walker()?;
     Ok(())
+}
+
+fn get_dir_walker() -> Result<WalkDir, io::Error> {
+    let current_dir = env::current_dir()?;
+    Ok(WalkDir::new(current_dir))
 }
