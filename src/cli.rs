1e17d4cc717decdb8ddbf9a7656af7e35f38912c
TornaxO7 <tornax@proton.me>
2023-09-01 21:32:53 +0200

    setup clap

---
+++ src/cli.rs
@@ -0,0 +1,6 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+mod cli;
+
+use clap::Parser;
+
 #[derive(thiserror::Error, Debug, Clone, PartialEq, Eq, Hash)]
-pub enum NucleError {
-}
+pub enum NucleError {}
 
 pub fn run() -> Result<(), NucleError> {
+    let args = cli::Cli::parse();
     Ok(())
 }
